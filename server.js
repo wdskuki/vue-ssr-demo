055913a3f0776640bd4001510756273ae5da874c
Dongsheng Wei <weidongsheng@dajiazhongyi.com>
2018-12-20 11:20:56 +0800

    update

--- server.js
+++ server.js
@@ -1,21 +1,14 @@
 /* server.js */
-// const createApp = require('./src/main')
-// import createApp from './src/main'
 const exp = require('express')
 const path = require('path')
 const express = exp()
-// const renderer = require('vue-server-renderer').createRenderer()
-// const createApp = require('./dist/server/bundle.server.js')['default']
-
-// const createApp = require('./dist/server/vue-ssr-server-bundle.json')
 
 const { createBundleRenderer } = require('vue-server-renderer')
 const bundle = require('./dist/server/vue-ssr-server-bundle.json')
 const renderer = createBundleRenderer(bundle) // can also directly pass the absolute path string.
 
 // 设置静态文件目录
-express.use('/', exp.static(path.join(__dirname, '/dist/server')))
-
+express.use('/', exp.static(path.join(__dirname, '/dist')))
 const clientBundleFileUrl = '/client/bundle.client.js'
 
 // 响应路由请求
@@ -25,7 +18,7 @@ express.get('*', (req, res) => {
   // 创建vue实例，传入请求路由信息
   renderer.renderToString(context, (err, html) => {
     if (err) {
-      console.error(err)
+      console.error(context, err, html)
       return res.state(500).end('运行时错误')
     }
     res.send(`
